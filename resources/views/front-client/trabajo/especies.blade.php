7aab81de6af76b94612a4e04ea4e9106120dd90e
aleixtorzap <aleixtorzap@campus.monlau.com>
2024-05-13 18:11:50 +0200

    imagenes

---
+++ resources/views/front-client/trabajo/especies.blade.php
@@ -0,0 +1,43 @@
+<x-app-layout>
+
+    <div>
+
+        <main class="pb-4">
+            <div class="bg-cover bg-center bg-no-repeat h-screen flex justify-center items-center flex-col"
+            style="background-image: url('../assets/images/especiesFondo.png');">
+            <p class="text-5xl text-white text-center ml-14" style="font-weight: bold;"><span >ESPE</span> <span style="color: #4CAF50;">CIES</span></p>
+        </div>
+        
+        </main>
+
+
+
+    </div>
+    <div class="mt-2 p-16">
+        <div class="flex items-center ">
+            <hr style="flex: 1; border-color: #4CAF50;">
+            <p class="m-0 text-2xl" style="color: #4CAF50">AYÚDANOS A CONSERVAR </p>
+            <hr style="flex: 1; border-color: #4CAF50;">
+        </div>
+        <div class="flex justify-center p-8 ">
+            <p class="text-center  text-2xl">
+                Únete a la mayor organización independiente de defensa de la naturaleza. Gracias al apoyo de nuestros
+                socios y socias podemos seguir luchando por la conservación de lugares únicos y especies en peligro en
+                todo el mundo. Al unirte a WWF estás ayudando a conservar especies amenazadas como el lince y el lobo
+                ibérico y a proteger bosques y océanos. ¡Contamos contigo!, algunos de esos ejemplos los puedes
+                encontrar aquí.
+            </p>
+        </div>
+    </div>
+
+    <div>
+        <div class="mt-2 p-16">
+            <div class="flex items-center ">
+                <hr style="flex: 1; border-color: #4CAF50;">
+                <p class="m-0 text-2xl" style="color: #4CAF50">NOTICIAS RECIENTES </p>
+                <hr style="flex: 1; border-color: #4CAF50;">
+            </div>
+           
+        </div>
+
+</x-app-layout>
